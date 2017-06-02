9f4466a9626b95a077f9e2fc32d69b1e23244b81
Susana Castela <susana.castela@itequia.com>
2017-06-02 17:28:01 +0200

    hello world

---
+++ ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Hubs
+{
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string ConnectionId { get; set; }
+    }
+
+    public static class UserHandler
+    {
+        public static List<User> ConnectedUsers = new List<User>();
+    }
+
+
+    public class ChatHub : Hub
+    {
+        public void Hello(string username)
+        {
+            Clients.All.hello(username);
+        }
+
+        public void Update(string username)
+        {
+            UserHandler.ConnectedUsers.Add(new User { Username = username, ConnectionId = Context.ConnectionId });
+            string usernames = string.Join(", ", UserHandler.ConnectedUsers.Select(u => u.Username));
+            Clients.All.update(usernames);
+        }
+
+        public void Send(string username, string message)
+        {
+            Clients.All.addNewMessageToPage(username, message);
+        }
+
+        public override Task OnDisconnected(bool d)
+        {
+            User userToRemove = UserHandler.ConnectedUsers.First(u => u.ConnectionId == Context.ConnectionId);
+            UserHandler.ConnectedUsers.Remove(userToRemove);
+            string usernames = string.Join(", ", UserHandler.ConnectedUsers.Select(u => u.Username));
+            Clients.All.update(usernames);
+            return base.OnDisconnected(true);
+        }
+    }
+}
---
+++ ChatSignalR/Scripts/Chat/chat.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+
+    $.ajax({
+        url: "/signalr/hubs",
+        dataType: "script",
+        async: false
+    });
+
+    function encodeHTML(s) {
+        return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+    }
+
+
+
+
+
+
+    //Chat logic:
+    var chat = $.connection.chatHub;
+
+    chat.client.addNewMessageToPage = function (username, message) {
+        $('#discussion').append('<li><strong>' + username + '</strong>' + message + '</li>');
+    };
+
+    chat.client.hello = function (username) {
+        $('#users').append('<li>' + username + '</li>');
+    };
+
+    chat.client.update = function (users) {
+        $('#users2').html(users);
+    };
+
+    $('#username').val(prompt('Tu nick de messenger:', ''));
+    $('#message').focus();
+
+    $.connection.hub.start().done(function () {
+
+        chat.server.hello($('#username').val());
+        chat.server.update($('#username').val());
+
+        $('#send').click(function () {
+            chat.server.send($('#username').val(),encodeHTML($('#message').val()));
+            $('#message').val('').focus();
+        })
+    });
+
+    
+});
